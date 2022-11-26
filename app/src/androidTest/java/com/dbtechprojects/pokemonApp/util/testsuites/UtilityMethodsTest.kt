d01aeb0c00cfbd498db5f531281d1ae3dcfe4875
Daniel Butler <danielmbutler@hotmail.co.uk>
2022-11-26 12:53:24 +0000

    converter test

--- app/src/androidTest/java/com/dbtechprojects/pokemonApp/util/testsuites/UtilityMethodsTest.kt
+++ app/src/androidTest/java/com/dbtechprojects/pokemonApp/util/testsuites/UtilityMethodsTest.kt
@@ -1,11 +1,16 @@
 package com.dbtechprojects.pokemonApp.util.testsuites
 
 
-import android.util.Log
+
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.dbtechprojects.pokemonApp.models.api_responses.Converters
+import com.dbtechprojects.pokemonApp.models.api_responses.NameAndUrl
+import com.dbtechprojects.pokemonApp.models.api_responses.PokemonStat
 import junit.framework.TestCase
 import org.junit.Test
 import org.junit.runner.RunWith
+
+
 
 // class to test room db
 
@@ -17,10 +22,10 @@ class UtilityMethodsTest : TestCase() {
      * @see com.dbtechprojects.pokemonApp.ui.viewmodels.DetailViewModel.plotLeft
      */
     @Test
-    fun randomPositioningTest(){
+    fun randomPositioningTest() {
         val randomInts = mutableListOf<Int>()
         // 0 to 20 is enough for our usecase
-        for (i in 0..20){
+        for (i in 0..20) {
             randomInts.add((0..600).random())
         }
         assertEquals(randomInts.size == randomInts.toSet().size, true)
@@ -29,12 +34,21 @@ class UtilityMethodsTest : TestCase() {
 
     //test random range
     @Test
-    fun randomRangeTest(){
+    fun randomRangeTest() {
         val randomInts = mutableListOf<Int>()
-        for (i in 0..200){
+        for (i in 0..200) {
             randomInts.add((0..600).random())
         }
         assertEquals(randomInts.filter { (0..600).contains(it) }.size == randomInts.size, true)
     }
+
+    @Test
+    fun typeConverterTest(){
+        val data = listOf(PokemonStat(baseStat = 6, effort = 6, stat = NameAndUrl("test", "test")))
+        val converters = Converters()
+        val string = converters.fromPokemonStat(data)
+        assertEquals(data == converters.toPokemonStat(string), true)
+    }
+
 
 }
